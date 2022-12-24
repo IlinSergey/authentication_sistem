66302843ac8478e660efefd913c95b86e5051144
IlinSergey <ilin_sergey90@mail.ru>
2022-12-24 17:06:37 +0300

    функция проверки логина/пароля

--- authdemo/server.py
+++ authdemo/server.py
@@ -4,6 +4,19 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
+users = {
+    'alexey@user.com': {
+        'name': 'Алексей',
+        'password': 'some_password_1',
+        'balance': 100_000
+    },
+    'petr@user.com': {
+        'name': 'Пётр',
+        'password': 'some_password_2',
+        'balance': 555_555
+    }
+}
+
 
 @app.get('/')
 def index_page():
@@ -14,7 +27,10 @@ def index_page():
 
 @app.post('/login')
 def process_login_page(username: str = Form(...), password: str = Form(...)):
-    return Response(f'Ваш логин: {username}, пароль: {password}', media_type='text/html')
+    user = users.get(username)
+    if not user or user['password'] != password:
+        return Response('Я вас не знаю!', media_type='text/html')
+    return Response(f"Привет {user['name']}!<br> Баланс: {user['balance']}", media_type='text/html')
 
 
 
