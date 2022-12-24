5df6d986945fc81b3fafced5d6d8b94a78c7bec6
IlinSergey <ilin_sergey90@mail.ru>
2022-12-24 16:41:25 +0300

    Принемаем данные с форм на сервере

--- authdemo/server.py
+++ authdemo/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 
@@ -10,6 +10,11 @@ def index_page():
     with open('templates/login.html', 'r') as f:
         login_page = f.read()
     return Response(login_page, media_type='text/html')
+
+
+@app.post('/login')
+def process_login_page(username: str = Form(...), password: str = Form(...)):
+    return Response(f'Ваш логин: {username}, пароль: {password}', media_type='text/html')
 
 
 
